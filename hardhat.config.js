98c38795533d45eb189ef15ecf1f4585f705d016
Tulun <jaykiraly@gmail.com>
2021-10-05 15:29:21 -0700

    trying to setup hardhat and opt.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@eth-optimism/plugins/hardhat/compiler");
+require("@eth-optimism/hardhat-ovm");
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      hardfork: "london",
+      gasPrice: "auto",
+      initialBaseFeePerGas: 1_000_000_000,
+    },
+    optimistic: {
+      url: "http://127.0.0.1:9545", // this is the default port
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+      gasPrice: 15_000_000, // required
+      ovm: true, // required
+    },
+  },
+};
