db954a32c6948f7235f41c133abe8e17eadffc6a
Tulun <jaykiraly@gmail.com>
2021-11-16 09:55:15 -0800

    add check for environment variables.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
+import assert from "assert";
+
+// App will have errors without these environment variables -- throw an error.
+assert(process.env.REACT_APP_PUBLIC_ONBOARD_API_KEY, "Onboard API Key must be defined.")
+assert(process.env.REACT_APP_PUBLIC_INFURA_ID, "Infura Public API Key must be defined.")
 
 function App() {
   return (
