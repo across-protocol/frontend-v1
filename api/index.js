7d12c2562def467346d2d124e6b3d6431c599ae2
Matt Rice <matthewcrice32@gmail.com>
2021-12-17 23:11:22 -0500

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,26 +1,40 @@
+// Note: ideally this would be written in ts as vercel claims they support it natively.
+// However, when written in ts, the imports seem to fail, so this is in js for now.
+
 
 const sdk = require("@uma/sdk-next");
 const { BridgeAdminEthers__factory } = require("@uma/contracts-node");
 const ethers = require("ethers");
 
 const handler = async (request, response) => {
-  const { REACT_APP_PUBLIC_INFURA_ID } = process.env;
-  const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${REACT_APP_PUBLIC_INFURA_ID}`);
+  try {
+    const { REACT_APP_PUBLIC_INFURA_ID } = process.env;
+    const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${REACT_APP_PUBLIC_INFURA_ID}`);
 
-  let { amount, l2Token, chainId } = request.query;
-  if (!isString(amount) || !isString(l2Token) || !isString(chainId)) throw new Error("Must provide amount and token as query params");
+    let { amount, l2Token, chainId } = request.query;
+    if (!isString(amount) || !isString(l2Token) || !isString(chainId)) throw new InputError("Must provide amount and token as query params");
 
-  let { l1Token } = await getTokenDetails(provider, l2Token, chainId);
-  if (l1Token === sdk.across.constants.ADDRESSES.WETH) l1Token = sdk.across.constants.ADDRESSES.WETH;
-  const depositFeeDetails = await sdk.across.gasFeeCalculator.getDepositFeesDetails(provider, amount, l1Token === sdk.across.constants.ADDRESSES.WETH ? sdk.across.constants.ADDRESSES.ETH : l1Token);
-  
+    let { l1Token } = await getTokenDetails(provider, l2Token, chainId);
+    if (l1Token === sdk.across.constants.ADDRESSES.WETH) l1Token = sdk.across.constants.ADDRESSES.WETH;
+    const depositFeeDetails = await sdk.across.gasFeeCalculator.getDepositFeesDetails(provider, amount, l1Token === sdk.across.constants.ADDRESSES.WETH ? sdk.across.constants.ADDRESSES.ETH : l1Token);
+    
+    const responseJson = {
+      slowFeePct: depositFeeDetails.slow.pct,
+      fastFeePct: depositFeeDetails.instant.pct
+    };
 
-  const responseJson = {
-    slowFeePct: depositFeeDetails.slow.pct,
-    fastFeePct: depositFeeDetails.instant.pct
-  };
-
-  response.status(200).json(responseJson);
+    response.status(200).json(responseJson);
+  } catch (error) {
+    if (error instanceof InputError) {
+      response.status(400).json({
+        error: error.message
+      });
+    } else {
+      response.status(500).json({
+        error: error.message
+      });
+    }
+  }
 };
 
 
@@ -31,9 +45,9 @@ const getTokenDetails = async (provider, l2Token, chainId) => {
   const l2TokenFilter = bridgeAdmin.filters.WhitelistToken(undefined, undefined, l2Token);
 
   // Filter events by chainId.
-  let events = (await bridgeAdmin.queryFilter(l2TokenFilter, 0, "latest")).filter((event) => event.args.chainId.toString() === chainId);
+  const events = (await bridgeAdmin.queryFilter(l2TokenFilter, 0, "latest")).filter((event) => event.args.chainId.toString() === chainId);
 
-  if (events.length === 0) throw new Error("No whitelisted token found");
+  if (events.length === 0) throw new InputError("No whitelisted token found");
 
   // Sorting from most recent to oldest.
   events.sort((a, b) => {
@@ -48,5 +62,7 @@ const getTokenDetails = async (provider, l2Token, chainId) => {
 }
 
 const isString = (input) => typeof input === "string";
+
+class InputError extends Error {}
 
 module.exports = handler;
