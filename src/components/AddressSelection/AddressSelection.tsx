bebbd07d6fa385259fe63aeea170c3a29513f155
amateima <amatei@umaproject.org>
2021-12-20 22:55:11 +0200

    Fix linter warnings
    Signed-off-by: amateima <amatei@umaproject.org>

--- src/components/AddressSelection/AddressSelection.tsx
+++ src/components/AddressSelection/AddressSelection.tsx
@@ -110,85 +110,91 @@ const AddressSelection: React.FC = () => {
                       ? "Mainnet"
                       : selectedItem?.name}
                   </ToggleChainName>
-                {toAddress && <Address>{shortenAddress(toAddress)}</Address>}
-              </div>
-              <ToggleIcon />
-            </ToggleButton>
-          </RoundBox>
-          <Menu isOpen={isOpen} {...getMenuProps()}>
-            {isOpen &&
-              sendState.currentlySelectedToChain.chainId !== ChainId.MAINNET &&
-              CHAINS_SELECTION.map((t, index) => {
-                return (
-                  <Item
-                    className={
-                      t === sendState.currentlySelectedToChain || t.chainId === ChainId.MAINNET ? "disabled" : ""
-                    }
-                    {...getItemProps({ item: t, index })}
-                    key={t.chainId}
-                  >
-                    <Logo src={t.logoURI} alt={t.name} />
-                    <div>{t.name}</div>
-                    <span className="layer-type">
-                      {t.chainId !== ChainId.MAINNET ? "L2" : "L1"}
-                    </span>
-                  </Item>
-                );
-              })}
-            {isOpen &&
-              sendState.currentlySelectedToChain.chainId ===
-                ChainId.MAINNET && (
-                <>
-                  <ItemWarning
-                    initial={{ y: -10 }}
-                    animate={{ y: 0 }}
-                    exit={{ y: -10 }}
-                  >
-                    <p>
-                      Transfers between L2 chains is not possible at this time
-                    </p>
-                  </ItemWarning>
-                  {CHAINS_SELECTION.map((t, index) => {
-                    return (
-                      <Item
-                        className={"disabled"}
-                        {...getItemProps({ item: t, index })}
-                        key={t.chainId}
-                        initial={{ y: -10 }}
-                        animate={{ y: 0 }}
-                        exit={{ y: -10 }}
-                      >
-                        <Logo src={t.logoURI} alt={t.name} />
-                        <div>{t.name}</div>
-                        <span className="layer-type">
-                          {index !== CHAINS_SELECTION.length - 1 ? "L2" : "L1"}
-                        </span>
-                      </Item>
-                    );
-                  })}
-                </>
-              )}
-          </Menu>
-        </InputGroup>
-        <ChangeWrapper onClick={toggle}>
-          <ChangeButton className={!isConnected ? "disabled" : ""}>
-            Change account
-          </ChangeButton>
-        </ChangeWrapper>
-      </Wrapper>
-      <Dialog isOpen={open} onClose={toggle}>
-        <h3>Send To</h3>
-        <div>Address on {CHAINS[toChain].name}</div>
-        <InputWrapper>
-          <Input onChange={handleChange} value={address} />
-          <ClearButton onClick={clearInput}>
-            <XOctagon
-              fill="var(--color-gray-300)"
-              stroke="var(--color-white)"
-            />
-          </ClearButton>
-          {!isValid && <InputError>Not a valid address</InputError>}
-        </InputWrapper>
+                  {toAddress && <Address>{shortenAddress(toAddress)}</Address>}
+                </div>
+                <ToggleIcon />
+              </ToggleButton>
+            </RoundBox>
+            <Menu isOpen={isOpen} {...getMenuProps()}>
+              {isOpen &&
+                sendState.currentlySelectedToChain.chainId !==
+                  ChainId.MAINNET &&
+                CHAINS_SELECTION.map((t, index) => {
+                  return (
+                    <Item
+                      className={
+                        t === sendState.currentlySelectedToChain ||
+                        t.chainId === ChainId.MAINNET
+                          ? "disabled"
+                          : ""
+                      }
+                      {...getItemProps({ item: t, index })}
+                      key={t.chainId}
+                    >
+                      <Logo src={t.logoURI} alt={t.name} />
+                      <div>{t.name}</div>
+                      <span className="layer-type">
+                        {t.chainId !== ChainId.MAINNET ? "L2" : "L1"}
+                      </span>
+                    </Item>
+                  );
+                })}
+              {isOpen &&
+                sendState.currentlySelectedToChain.chainId ===
+                  ChainId.MAINNET && (
+                  <>
+                    <ItemWarning
+                      initial={{ y: -10 }}
+                      animate={{ y: 0 }}
+                      exit={{ y: -10 }}
+                    >
+                      <p>
+                        Transfers between L2 chains is not possible at this time
+                      </p>
+                    </ItemWarning>
+                    {CHAINS_SELECTION.map((t, index) => {
+                      return (
+                        <Item
+                          className={"disabled"}
+                          {...getItemProps({ item: t, index })}
+                          key={t.chainId}
+                          initial={{ y: -10 }}
+                          animate={{ y: 0 }}
+                          exit={{ y: -10 }}
+                        >
+                          <Logo src={t.logoURI} alt={t.name} />
+                          <div>{t.name}</div>
+                          <span className="layer-type">
+                            {index !== CHAINS_SELECTION.length - 1
+                              ? "L2"
+                              : "L1"}
+                          </span>
+                        </Item>
+                      );
+                    })}
+                  </>
+                )}
+            </Menu>
+          </InputGroup>
+          <ChangeWrapper onClick={toggle}>
+            <ChangeButton className={!isConnected ? "disabled" : ""}>
+              Change account
+            </ChangeButton>
+          </ChangeWrapper>
+        </Wrapper>
+        <Dialog isOpen={open} onClose={toggle}>
+          <h3>Send To</h3>
+          <div>Address on {CHAINS[toChain].name}</div>
+          <InputWrapper>
+            <Input onChange={handleChange} value={address} />
+            <ClearButton onClick={clearInput}>
+              <XOctagon
+                fill="var(--color-gray-300)"
+                stroke="var(--color-white)"
+              />
+            </ClearButton>
+            {!isValid && <InputError>Not a valid address</InputError>}
+          </InputWrapper>
 
           <ButtonGroup>
             <CancelButton onClick={toggle}>Cancel</CancelButton>
