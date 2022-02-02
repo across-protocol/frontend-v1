7f0459130f96b32ab07b8210a46a69bdaa347ecc
Tom <tom@TOms-MacBook-Air.local>
2022-02-02 09:09:05 -0500

    updates to pageview analytics

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,19 +1,3 @@
-<!-- Matomo -->
-<script>
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://across.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='//cdn.matomo.cloud/across.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
-
 export { default as Send } from "./Send";
 export { default as Pool } from "./Pool";
 export { default as About } from "./About";
